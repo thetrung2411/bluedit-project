3df77c645be54886efbf0e78b0d9a281292e8e10
thetrung2411 <thetrung2411@gmail.com>
2019-08-14 19:45:06 +1000

    feat-post-UI

--- src/components/post/PostLayout.jsx
+++ src/components/post/PostLayout.jsx
@@ -1,25 +1,84 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import {Typography, FormControl, FormControlLabel} from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {Typography, FormControl, FormControlLabel, IconButton} from '@material-ui/core';
+import { postLayoutStyles } from "./PostLayoutStyle";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';  
+import NavigationIcon from '@material-ui/icons/Navigation';
+import EditRounded from '@material-ui/icons/EditRounded';
 function PostLayout(props){
+  const [open, setOpen] = React.useState(false);
+  const {classes} = props;
+
+  function handleClickOpen() {
+    console.log("asdasdasd");
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
     return (
         <div>
-       <FormControl>
-        <TextField
-        id="outlined-multiline-flexible"
-        label="Post here"
-        multiline
-        rowsMax="1000"
-        margin="normal"
-        variant="outlined"
-      />
-      </FormControl>
-       
-       <Button variant="contained" color="primary">
-        Submit
-      </Button>
+          <Fab color="primary" onClick={() => {setOpen(true);}}>
+        <EditRounded/>
+      </Fab>
+      
+           
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+        className = {classes.paper}
+      >
+        <DialogTitle id="responsive-dialog-title" align = "center">Post</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To submit your post please enter the text field below
+          </DialogContentText>
+                         <TextField
+                         id="outlined-multiline-flexible"
+                          label="Post here"
+                          multiline
+                          rows = "7"
+                          rowsMax="1000"
+                          margin="none"
+                          variant="outlined"
+                          className = {classes.text}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={handleClose}
+                        color = "secondary"
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color = "primary"
+                        onClick={handleClose}
+                    >
+                        Submit
+                    </Button>
+                    
+        
+        </DialogActions>
+      </Dialog>
      </div>
     );
 }
-export default PostLayout;
+export default withStyles (postLayoutStyles)(PostLayout);
---
+++ src/components/post/PostLayoutStyle.js
@@ -0,0 +1,62 @@
+
+
+export const postLayoutStyles = theme => ({
+    background: {
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "black",   
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        position: "relative"
+    },
+    main: {
+        width: "80vw",
+        display: "block", // Fix IE 11 issue.
+        marginLeft: "auto",
+        marginRight: "auto",
+        paddingTop: "10vh",
+        [theme.breakpoints.up("md")]: {
+            width: 700
+        },
+        backgroundColor: "white",
+    },
+    paper: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit *
+            3}px ${theme.spacing.unit * 3}px`
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+        marginBottom: theme.spacing.unit
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3
+    },
+    divider: {
+        height: theme.spacing.unit / 6,
+        backgroundColor: "#8c8c8c",
+        width: "80%",
+        marginTop: theme.spacing.unit * 2
+    },
+    button: {
+        alignSelf: "start",
+        backgroundColor: "#e5e5e5",
+        borderRadius: "5px",
+        padding: "0 5px 4px",
+        color: "red"
+    },
+    
+    text: {
+        width: "100%",
+        height: "200",
+        
+    },
+});
