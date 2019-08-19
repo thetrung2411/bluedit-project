137c61913ed5fb12145d0ac0733eb52d7ace9d83
CalvinSup <wjl494280637@gmail.com>
2019-08-19 15:53:21 +1000

    test

--- src/components/search/searching.jsx
+++ src/components/search/searching.jsx
@@ -3,5 +3,16 @@ import Grid from "@material-ui/core/Grid";
 import AppBar from "../appBar/appBar";
 
 function searching (props) {
-
+    return (
+        <div>
+            <AppBar />
+            <Grid container spacing = {3} >
+            <Grid item xs={8}><PostItem/></Grid>
+            <Grid item xs={4}><RecommendationItem/></Grid>
+            </Grid>
+            
+        </div>
+        
+       
+    );
 }
