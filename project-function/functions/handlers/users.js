0267d291f458d1ec46f6aed474f97e57d6ba25a0
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2019-08-25 16:47:07 +1000

    api update signup and implement login

---
+++ project-function/functions/handlers/users.js
@@ -0,0 +1,85 @@
+const { db, admin } = require("../util/admin");
+
+const config = require("../util/config");
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+exports.signup = (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userName: req.body.userName
+  };
+
+  const noImg = "no-img.png";
+
+  let token, userId;
+
+  db.doc("/users/" + newUser.userName)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({
+          userName: "This user name is already taken"
+        });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        userName: newUser.userName,
+        email: newUser.email,
+        createdAt: newUser.email,
+        createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+        userId
+      };
+      return db.doc(`/users/${newUser.userName}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
+      }
+    });
+};
+
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      return res
+        .status(403)
+        .json({ general: "Wrong user detail, please try again" });
+    });
+};
