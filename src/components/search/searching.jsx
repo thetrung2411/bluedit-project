d43592f47bf1f04aa5154e4bf8da876d4e09b029
CalvinSup <wjl494280637@gmail.com>
2019-08-26 15:55:01 +1000

    test

--- src/components/search/searching.jsx
+++ src/components/search/searching.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import AppBar from "../appBar/appBar";
+import SearchPost from "..search/SearchPost";
 
-function searching (props) {
+function searching(props) {
     return (
         <div>
             <AppBar />
             <Grid container spacing = {3} >
-            <Grid item xs={8}><PostItem/></Grid>
-            <Grid item xs={4}><RecommendationItem/></Grid>
+            <Grid item xs={8}><SearchItem/></Grid>
+            <Grid item xs={4}><SearchPost/></Grid>
             </Grid>
             
         </div>
@@ -16,3 +17,4 @@ function searching (props) {
        
     );
 }
+export default searching;
