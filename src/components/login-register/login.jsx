4cc5192dee7e5ae95c06399010ad49405ac96a49
Mike0298 <38065113+Mike0298@users.noreply.github.com>
2019-08-28 12:07:51 +1000

    Add proxy, login page now connected to backend, install axios and redux for future usage

--- src/components/login-register/login.jsx
+++ src/components/login-register/login.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 //MUI import
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles = {
+  typography: {
+    useNextVariants: true
+  },
   form: {
     textAlign: "center"
+  },
+  image: {
+    margin: "20px auto 20px auto"
   },
   pageTitle: {
     margin: "10px auto 10px auto"
@@ -21,24 +29,28 @@ const styles = {
   },
   button: {
     marginTop: 20,
-    marginBottom: 20
+    position: "relative"
   },
   customError: {
     color: "red",
     fontSize: "0.8rem",
     marginTop: 10
   },
-  link: {
-    color:'white'
+  progress: {
+    position: "absolute"
   }
 };
 
 class login extends Component {
-  state = {
-    email: "",
-    password: "",
-    errors: {}
-  };
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+      loading: false
+    };
+  }
 
   handleChange = e => {
     this.setState({
@@ -46,11 +58,35 @@ class login extends Component {
     });
   };
 
-  handleSubmit = e => {};
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post("/login", userData)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          loading: false
+        });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        });
+      });
+  };
 
   render() {
     const { classes } = this.props;
-    const { errors } = this.state;
+    const { errors, loading } = this.state;
 
     return (
       <Grid container className={classes.form}>
@@ -68,6 +104,8 @@ class login extends Component {
               type="email"
               label="Email"
               className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
               value={this.state.email}
               onChange={this.handleChange}
               fullWidth
@@ -77,6 +115,9 @@ class login extends Component {
               name="password"
               type="password"
               label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
               value={this.state.password}
               onChange={this.handleChange}
               fullWidth
@@ -86,23 +127,32 @@ class login extends Component {
                 {errors.general}
               </Typography>
             )}
-            <Link to ="/post">
             <Button
               type="submit"
               variant="contained"
               color="primary"
               className={classes.button}
+              disabled={loading}
             >
               Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
             </Button>
-            </Link>
+            <br />
             <br />
             <Typography variant="subtitle2">
-              Don't have an account? Sign up <Link to="/register" className={classes.link}>here</Link>
+              Don't have an account? Sign up{" "}
+              <Link to="/register" className={classes.link}>
+                here
+              </Link>
             </Typography>
             <br />
             <Typography variant="subtitle2">
-              Or go back to <Link to="/" className={classes.link}>Bluedit</Link>
+              Or go back to{" "}
+              <Link to="/" className={classes.link}>
+                Bluedit
+              </Link>
             </Typography>
           </form>
         </Grid>
