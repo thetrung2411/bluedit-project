220e75dc831ee187c9db59feb62225e22c20ccff
CalvinSup <wjl494280637@gmail.com>
2019-09-28 17:14:33 +1000

    t

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -17,7 +17,7 @@
  
  
  
- export class HomeAppBar extends React.Component {
+ export class seatch extends React.Component {
      state = {
          value: "",
          name: "",
@@ -142,6 +142,62 @@
  export default connect(
      mapStateToProps,
      mapActionsToProps
- )(withStyles(appBarStyles)(HomeAppBar));
+ )(withStyles(appBarStyles)(search));
  
- 
+//  import React, { Component } from "react";
+// import AppBar from "../appBar/appBar";
+// import PostItems from "../post/PostItems";
+// import Grid from "@material-ui/core/Grid";
+// import RecommendationItem from "../post/Recommendation";
+// import CircularProgress from "@material-ui/core/CircularProgress";
+// import axiosConfig from "../../axiosConfig";
+// import { connect } from 'react-redux';
+// import { getAllPosts } from '../../redux/actions/postActions';
+
+// export class HomePageLayout extends Component {
+//   state = {
+//     post: null
+//   };
+//   componentDidMount() {
+//     this.props.getAllPosts();
+//   }
+//   render() {
+
+//     const { post, posts } = this.props;
+//     let postMarkUp = post.length ? (
+//       post.map(item => <PostItems post={item} />)
+//     ) : posts.length ? (
+//       posts.map(item => <PostItems post={item} />)
+//     ) : <CircularProgress color="inherit" />;
+//     return (
+//       <div>
+//         <AppBar />
+//         <Grid container spacing={3}>
+//           <Grid item xs={8}>
+//             {postMarkUp}
+//           </Grid>
+//           <Grid item xs={4}>
+//             <RecommendationItem />
+//           </Grid>
+//         </Grid>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   console.log('state', state)
+//   return {
+//     post: state.post.post,
+//     posts: state.post.posts,
+//   };
+// };
+
+// const mapActionsToProps = {
+//   getAllPosts
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   mapActionsToProps
+// )(HomePageLayout);
