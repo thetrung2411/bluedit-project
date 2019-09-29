647eff50728588dccb62fc7b78d0b68af88d450e
ria02 <yuejia.zhu.ria@gmail.com>
2019-09-29 12:22:40 +1000

    implemented ads page, ad table

--- src/components/ads/AdsPage.jsx
+++ src/components/ads/AdsPage.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
 import AppBarWithAvatar from "../appBar/AppBarWithAvatar";
-
+import axiosConfig from "../../axiosConfig";
 import { withRouter, Route, Switch } from "react-router-dom";
 
+//Components
+import AdsTable from "./AdsTable";
+
+//MUI
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 export class AdsPage extends Component {
+  state = {
+    ads: null
+  };
+  componentDidMount() {
+    axiosConfig
+      .get("/getAllAds")
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          ads: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    let showAds = this.state.ads ? (
+      <AdsTable ads={this.state.ads} />
+    ) : (
+      <CircularProgress />
+    );
+
     return (
       <div>
         <AppBarWithAvatar />
         <h1>Advertisements</h1>
+        <p>{showAds}</p>
       </div>
     );
   }
---
+++ src/components/ads/AdsTable.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+//material ui
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
+//import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined";
+import DeleteForeverOutlinedIcon from "@material-ui/icons/DeleteForeverOutlined";
+
+const head = (
+  <TableHead>
+    <TableRow>
+      <TableCell>Advertisement ID</TableCell>
+      <TableCell>Uploaded At</TableCell>
+      <TableCell>Display</TableCell>
+      <TableCell></TableCell>
+    </TableRow>
+  </TableHead>
+);
+
+const AdTable = props => {
+  const allAds = props.ads.length ? (
+    props.ads.map(ad => {
+      return (
+        <TableBody>
+          <TableRow>
+            <TableCell align="left">{ad.adId}</TableCell>
+            <TableCell align="left">{ad.uploadAt}</TableCell>
+            <TableCell align="left">{ad.isShowing.toString()}</TableCell>
+            <TableCell>
+              <Button>View</Button>
+              <Button>
+                <DeleteForeverOutlinedIcon />
+              </Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      );
+    })
+  ) : (
+    <p className="canter">No advertisement found at the moment</p>
+  );
+  return (
+    <Table>
+      {head}
+      {allAds}
+    </Table>
+  );
+};
+
+export default AdTable;
