ba0cfe950bca399ab745373a0257a825fcda8359
ria02 <yuejia.zhu.ria@gmail.com>
2019-09-30 15:47:51 +1000

    added authorization

--- project-function/functions/index.js
+++ project-function/functions/index.js
@@ -91,19 +91,20 @@ app.post("/post/:postId/hide", FBAuth, hidePost);
 app.post("/post/:postId/unhide", FBAuth, unhidePost);
 app.get("/searchPost", SearchPost);
 app.get("/blackuser", BlockPosts);
+
 //Report
-app.get("/getAllReports", getAllReports);
-app.get("/getReport/:reportId", getReport);
-app.post("/changeStatus/:reportId", changeReportStatus);
-app.delete("/deleteReport/:reportId", deleteReport);
+app.get("/getAllReports", FBAuth, getAllReports);
+app.get("/getReport/:reportId", FBAuth, getReport);
+app.post("/changeStatus/:reportId", FBAuth, changeReportStatus);
+app.delete("/deleteReport/:reportId", FBAuth, deleteReport);
 
 //Ads
-app.post("/uploadAd", uploadAd);
-app.post("/adImage/:adId", uploadImage);
-app.post("/editAd/:adId", editAd);
-app.get("/getAllAds", getAllAds);
+app.post("/uploadAd", FBAuth, uploadAd);
+app.post("/adImage/:adId", FBAuth, uploadImage);
+app.post("/editAd/:adId", FBAuth, editAd);
+app.get("/getAllAds", FBAuth, getAllAds);
 app.get("/getAd/:adId", getAd);
-app.delete("/deleteAd/:adId", deleteAd);
+app.delete("/deleteAd/:adId", FBAuth, deleteAd);
 
 //Bookmark
 app.post("/bookmark", FBAuth, bookmark);
