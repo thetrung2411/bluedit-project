df7757f02e7fdc645bb0c8540f711b3e571c3fcd
Anzi Wu <12796064@student.uts.edu.au>
2019-10-13 00:08:44 +1100

    Update userPost.jsx

--- src/components/userpost/userPost.jsx
+++ src/components/userpost/userPost.jsx
@@ -2,63 +2,95 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
-// import PostItems from "./PostItems";
 import PostItems from "./../post/PostItems";
+import SubscribedUserTable from "./SubscribedUserTable";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { connect } from "react-redux";
 import { getAllPosts, postSubscribe } from "../../redux/actions/postActions";
 import { getSubscribe } from "./../../redux/actions/subscribeAction";
 import { Redirect } from "react-router-dom";
-import dayjs from "dayjs";
+
 
 export class UserPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isother: false,
+      name: ''
+    };
+  }
+
   componentDidMount() {
+    var name = this.GetQueryString('name')
+    if (name) {
+        this.setState({
+            isother: true,
+            name: name,
+        });
+    }
     this.props.getAllPosts();
     this.props.getSubscribe();
   }
 
+  GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
   render() {
     const { posts, loading } = this.props.post;
+    const { isother, name } = this.state;
     const {
       user: { authenticated }
     } = this.props;
 
 
-    const mypost = posts.filter(item => item.userPosted == this.props.user.userDetails.userName)
-    const mysubuser = this.props.post.subscribes.filter(item => item.subscriber == this.props.user.userDetails.userName)
-    const mysubs = []
-    mysubuser.forEach(item => {
-      posts.forEach(pitem => {
-        if (pitem.userPosted == item.userName) {
-          mysubs.push(pitem)
+    const myPost = posts.filter(item => item.userPosted === this.props.user.userDetails.userName)
+    const mySubscribeUser = this.props.post.subscribes.filter(item => item.subscriber === this.props.user.userDetails.userName)
+    const mySubscribedPost = []
+    mySubscribeUser.forEach(item => {
+      posts.forEach(postItem => {
+        if (postItem.userPosted === item.userName) {
+          mySubscribedPost.push(postItem)
         }
       })
     })
 
-    const allpost = mypost.concat(mysubs)
+    const otherUserpost = posts.filter(item => item.userPosted === name)
 
-
+    const allPost = !name ? myPost.concat(mySubscribedPost) : otherUserpost;
 
     if (!authenticated) return <Redirect to="/home" />
-    let postMarkUp = !loading ? (
-      allpost.map(post => <PostItems key={post.postId} subscribes={this.props.subscribes}
-        postSubscribe={this.props.postSubscribe} post={post} />)
+    let post = !loading ? (
+      allPost.length ?
+      allPost.map(post => <PostItems key={post.postId} subscribes={this.props.subscribes}
+          postSubscribe={this.props.postSubscribe} post={post} />) :
+        <div style={{ width: '70%', textAlign: 'left', position: "absolute", top: "200px", left: "300px",}}>
+          No Posts
+        </div>
     ) : (
         <CircularProgress color="inherit" />
       );
+
     return (
       <div>
-        <div style={{ width: '70%', textAlign: 'left', margin: '0 auto' }}>
-          <Grid container spacing={3}>
-            <Grid container xs={8}>
-              {postMarkUp}
-            </Grid>
+        <Grid container spacing={3}>
+          <Grid container xs={8}>
+            <div style={{ width: '70%', textAlign: 'left', margin: '0 auto' }}>
+              {post}
+            </div>
           </Grid>
-        </div>
+          <Grid item xs={4}>
+            <SubscribedUserTable />
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
+
 UserPost.propTypes = {
   getAllPosts: PropTypes.func.isRequired,
   postSubscribe: PropTypes.func.isRequired,
