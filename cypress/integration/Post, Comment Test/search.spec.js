908090b9bb16f453721ed63cb2c021066ac4af9d
CalvinSup <wjl494280637@gmail.com>
2019-10-17 22:12:58 +1100

    t

--- cypress/integration/Post, Comment Test/search.spec.js
+++ cypress/integration/Post, Comment Test/search.spec.js
@@ -3,17 +3,14 @@ describe('Testing search', function() {
         cy.visit('https://asd-bluedit.herokuapp.com/searching')
     })
     it('Testing search', function() {
-        cy.get('#name').type('Test User').should('have.value', 'Test User')
         cy.get('search').click()
         cy.wait(5000);
     })
     it('Testing search', function() {
-        cy.get('#value').type('123').should('have.value', '123')
         cy.get('#search').click()
         cy.wait(5000);
     })
     it('Testing search', function() {
-        cy.get('#input block name').type('Test User').should('have.value', 'Test User')
         cy.get('#block').click()
         cy.wait(5000);
 })
