8f9a1adfcc38bad168ddef310c707ad0851a1396
CalvinSup <wjl494280637@gmail.com>
2019-10-17 20:04:43 +1100

    t

--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -3,10 +3,10 @@ import {shallow} from 'enzyme';
 import Searching from  './searching';
 import Search from './search';
 import { Provider } from "react-redux";
-import postReducers from "../../redux/reducers/postReducers";
 import configureMockStore from "redux-mock-store";
-import thunk from "redux-thunk"
+import postReducers from "../../redux/reducers/postReducers";
 import * as types from "../../redux/types";
+import thunk from "redux-thunk";
 
 const mockStore = configureMockStore([thunk]);
 const store = mockStore({});
@@ -24,37 +24,43 @@ describe('Search Component Test', () => {
         const component = shallow (<Provider store ={store}><Search/></Provider>);
         expect(component.find(Search).length).toBe(1);
     })
-    // test('Should return initial state', () => {
-    //     expect(postReducers(undefined, {})).toEqual(
-    //         {
-    //                 posts: [],
-    //                 post: {},
-    //                 loading: false
-    //         }
-    //     )
-    // })
-    // test('Should return GET_POSTS state', () => {
-    //     expect(postReducers({},{
-    //         type: types.GET_POSTS,     
-    //     })).toEqual({
-    //         posts: undefined,
-    //         loading: false
-    //     })
-    // })
-    // test('Should return GET_POST state', () => {
-    //     expect(postReducers({},{
-    //         type: types.GET_POST,     
-    //     })).toEqual({
-    //         post: undefined,
-    //     })
-    // })
+    test('Should return initial state', () => {
+        expect(postReducers(undefined, {})).toEqual(
+            {
+                    posts: [],
+                    post: {},
+                    loading: false,
+                    unSubscribes: [],
+                    subscribes: []
+            }
+        )
+    })
+    test('Should return GET_POSTS state', () => {
+        expect(postReducers({},{
+            type: types.GET_POSTS,     
+        })).toEqual({
+            posts: undefined,
+            loading: false,
+            unSubscribes: undefined,
+            subscribes: undefined
+        })
+    })
+    test('Should return GET_POST state', () => {
+        expect(postReducers({},{
+            type: types.GET_POST,     
+        })).toEqual({
+            post: undefined,
+        })
+    })
     test('Should return POST_POST state', () => {
         expect(postReducers(undefined,{
             type: types.POST_POST,     
         })).toEqual({
                 posts: [undefined],
                 post: {},
-                loading: false
+                loading: false,
+                unSubscribes: [],
+                subscribes: []
         })
     })
 })
