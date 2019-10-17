4dd5b5a362f87190c67e0db045a97858d6d0c8d1
CalvinSup <wjl494280637@gmail.com>
2019-10-17 18:24:10 +1100

    t

--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -3,8 +3,13 @@ import {shallow} from 'enzyme';
 import Searching from  './searching';
 import Search from './search';
 import { Provider } from "react-redux";
+import postReducers from "../../redux/reducers/postReducers";
 import configureMockStore from "redux-mock-store";
-const mockStore = configureMockStore();
+import thunk from "redux-thunk"
+import * as types from "../../redux/types";
+
+configure({ adapter: new Adapter() });
+const mockStore = configureMockStore([thunk]);
 const store = mockStore({});
 
 describe('Search page Component test', () => {
@@ -13,9 +18,44 @@ describe('Search page Component test', () => {
         expect(component.find(Searching).length).toBe(1);
     })
 })
-describe('Search page Component test', () => {
+
+describe('Search Component Test', () => {
+    
     test('Should render without errors', () => {
         const component = shallow (<Provider store ={store}><Search/></Provider>);
         expect(component.find(Search).length).toBe(1);
+    })
+    test('Should return initial state', () => {
+        expect(postReducers(undefined, {})).toEqual(
+            {
+                    posts: [],
+                    post: {},
+                    loading: false
+            }
+        )
+    })
+    test('Should return GET_POSTS state', () => {
+        expect(postReducers({},{
+            type: types.GET_POSTS,     
+        })).toEqual({
+            posts: undefined,
+            loading: false
+        })
+    })
+    test('Should return GET_POST state', () => {
+        expect(postReducers({},{
+            type: types.GET_POST,     
+        })).toEqual({
+            post: undefined,
+        })
+    })
+    test('Should return POST_POST state', () => {
+        expect(postReducers(undefined,{
+            type: types.POST_POST,     
+        })).toEqual({
+                posts: [undefined],
+                post: {},
+                loading: false
+        })
     })
 })
