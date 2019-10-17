305107544c700e76675ba68d258450ceaea86802
Anzi Wu <12796064@student.uts.edu.au>
2019-10-18 01:16:55 +1100

    Update PostMenu.jsx

--- src/components/post/PostMenu.jsx
+++ src/components/post/PostMenu.jsx
@@ -7,11 +7,7 @@ import MoreIcon from "@material-ui/icons/MoreVert";
 import EditButton from "./EditButton";
 import AddBookmark from "../bookmark/Addbookmark";
 import MenuItem from "@material-ui/core/MenuItem";
-import { setOnSubscribe, getSubscribe, getUnsubscribe } from "./../../redux/actions/subscribeAction";
-import SubscribeButton from "./SubscrbeButton";
-import { PostItemStyles } from "./PostItemsStyles";
-import { connect } from "react-redux";
-import withStyles from "@material-ui/core/styles/withStyles";
+
 class PostMenu extends Component {
     state = {
         anchorEl: null,
@@ -52,7 +48,6 @@ class PostMenu extends Component {
                   postId={postId}
                   userPosted={userPosted}
                 ></AddBookmark>
-       {!post.isMyPost && <SubscribeButton post={post} userDetails={user.userDetails} subscribes={subscribes} />}
        {hideButton}
        {editButton}
        {deleteButton}
@@ -63,37 +58,4 @@ class PostMenu extends Component {
     }
 }
 
-const mapStateToProps = state => {
-  const posts = state.post.posts
-  const subscribes = state.post.subscribes
-  const userDetails = state.user.userDetails;
-
-  subscribes.forEach(item => {
-    posts.forEach(postItem => {
-      if (postItem.userPosted === item.userName) {
-        postItem.isSub = true;
-      }
-    })
-  })
-
-  posts.forEach(postItem => {
-    if (postItem.userPosted === userDetails.userName) {
-      postItem.isMyPost = true;
-    }
-  })
-
-  return {
-    user: state.user
-  }
-}
-
-const mapActionsToProps = {
-  setOnSubscribe,
-  getSubscribe,
-  getUnsubscribe,
-};
-
-export default connect(
-  mapStateToProps,
-  mapActionsToProps
-)(withStyles(PostItemStyles)(PostMenu));
+export default PostMenu;
