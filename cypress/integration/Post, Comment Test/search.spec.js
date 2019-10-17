30c3d7ff19e92bd1c90319b553aee98bc2dee417
CalvinSup <wjl494280637@gmail.com>
2019-10-17 21:11:37 +1100

    t

--- cypress/integration/Post, Comment Test/search.spec.js
+++ cypress/integration/Post, Comment Test/search.spec.js
@@ -1,17 +1,18 @@
 describe('Testing search', function() {
+    beforeEach(function() {
         cy.visit('https://asd-bluedit.herokuapp.com/searching')
+    })
+    it('Testing search', function() {
         cy.get('#name Search…').type('Test User').should('have.value', 'Test User')
         cy.get('search').click()
         cy.wait(5000);
     })
-describe('Testing search', function() {
-    cy.visit('https://asd-bluedit.herokuapp.com/searching')
+    it('Testing search', function() {
         cy.get('#body Search…').type('123').should('have.value', '123')
         cy.get('#search').click()
         cy.wait(5000);
     })
-describe('Testing search', function() {
-    cy.visit('https://asd-bluedit.herokuapp.com/searching')
+    it('Testing search', function() {
         cy.get('#input block name').type('Test User').should('have.value', 'Test User')
         cy.get('#block').click()
         cy.wait(5000);
