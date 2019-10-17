7fa921b7081362dbeb91d607b0674d75bb1ecb78
Anzi Wu <12796064@student.uts.edu.au>
2019-10-17 21:42:00 +1100

    Update PostMenu.jsx

--- src/components/post/PostMenu.jsx
+++ src/components/post/PostMenu.jsx
@@ -7,6 +7,11 @@ import MoreIcon from "@material-ui/icons/MoreVert";
 import EditButton from "./EditButton";
 import AddBookmark from "../bookmark/Addbookmark";
 import MenuItem from "@material-ui/core/MenuItem";
+import { setOnSubscribe, getSubscribe, getUnsubscribe } from "./../../redux/actions/subscribeAction";
+import SubscribeButton from "./SubscrbeButton";
+import { PostItemStyles } from "./PostItemsStyles";
+import { connect } from "react-redux";
+import withStyles from "@material-ui/core/styles/withStyles";
 class PostMenu extends Component {
     state = {
         anchorEl: null,
@@ -19,7 +24,7 @@ class PostMenu extends Component {
         this.setState({open: false})
       }
     render(){
-        const {postId , userName, commentId, userPosted, body, hidden, commentHide } = this.props;
+        const {postId , userName, commentId, userPosted, body, hidden, commentHide, post, user, subscribes} = this.props;
         const deleteButton = userPosted === userName ? (
        <DeleteButton commentId={commentId} postId={postId} />
      ) : null;
@@ -47,6 +52,7 @@ class PostMenu extends Component {
                   postId={postId}
                   userPosted={userPosted}
                 ></AddBookmark>
+       {!post.isMyPost && <SubscribeButton post={post} userDetails={user.userDetails} subscribes={subscribes} />}
        {hideButton}
        {editButton}
        {deleteButton}
@@ -57,4 +63,37 @@ class PostMenu extends Component {
     }
 }
 
-export default PostMenu
+const mapStateToProps = state => {
+  const posts = state.post.posts
+  const subscribes = state.post.subscribes
+  const userDetails = state.user.userDetails;
+
+  subscribes.forEach(item => {
+    posts.forEach(postItem => {
+      if (postItem.userPosted === item.userName) {
+        postItem.isSub = true;
+      }
+    })
+  })
+
+  posts.forEach(postItem => {
+    if (postItem.userPosted === userDetails.userName) {
+      postItem.isMyPost = true;
+    }
+  })
+
+  return {
+    user: state.user
+  }
+}
+
+const mapActionsToProps = {
+  setOnSubscribe,
+  getSubscribe,
+  getUnsubscribe,
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(PostItemStyles)(PostMenu));
