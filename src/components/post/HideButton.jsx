6346486ec37bcb03a0cb34b047e7dd766336b2be
CalvinSup <wjl494280637@gmail.com>
2019-10-17 19:02:25 +1100

    m

--- src/components/post/HideButton.jsx
+++ src/components/post/HideButton.jsx
@@ -8,25 +8,12 @@ import {PostLayoutStyles } from "./PostLayoutStyle";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "@material-ui/core/Button";
 import {connect} from 'react-redux';
-<<<<<<< HEAD
-import {hidePost, unhidePost} from '../../redux/actions/postActions';
-import {hideComment, unhideComment} from '../../redux/actions/commentActions';
-=======
 import {deletePost} from '../../redux/actions/postActions';
 import {deleteComment} from '../../redux/actions/commentActions';
->>>>>>> master
 import PropTypes from 'prop-types';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-<<<<<<< HEAD
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-class HideButton extends Component{
-    state = {
-        openDialog: false,
-      };
-=======
 import Delete from '@material-ui/icons/Delete';
 class DeleteButton extends Component{
     state = {
@@ -35,36 +22,10 @@ class DeleteButton extends Component{
         errors: {} 
       };
 
->>>>>>> master
     handleOpen = () => {
       this.setState({openDialog: true});
     }
     handleClose = () => {
-<<<<<<< HEAD
-      this.setState({openDialog: false})
-    }
-  handleHide = () => {
-      if(this.props.commentId === undefined)
-        !this.props.hidden ? this.props.hidePost(this.props.postId) : this.props.unhidePost(this.props.postId)
-      else
-        !this.props.commentHide ? this.props.hideComment(this.props.postId, this.props.commentId) : this.props.unhideComment(this.props.postId, this.props.commentId) 
-      
-      this.handleClose();
-  }
-    render(){
-      const {hidden, commentHide} = this.props;
-      let visibilityButton;
-      let hideItem = <MenuItem  onClick={this.handleOpen}><ListItemIcon ><Visibility/></ListItemIcon><ListItemText primary="Hide" /></MenuItem>;
-      let unhideItem = <MenuItem  onClick={this.handleOpen}><ListItemIcon ><VisibilityOff/></ListItemIcon><ListItemText primary="Unhide" /></MenuItem>;
-      if(this.props.commentId === undefined)
-         visibilityButton = (!hidden) ? hideItem : unhideItem
-      else
-        visibilityButton = (!commentHide) ? hideItem : unhideItem
-      
-        return (
-            <div>
-             {visibilityButton}
-=======
         
       this.setState({openDialog: false})
     }
@@ -85,7 +46,6 @@ class DeleteButton extends Component{
               <MenuItem selected classes={{ selected: classes.menuItemDelete }} onClick={this.handleOpen} >
               <ListItemIcon ><Delete/></ListItemIcon> <ListItemText primary="Delete Post" />
           </MenuItem>
->>>>>>> master
           <Dialog
             open={this.state.openDialog}
             aria-labelledby="responsive-dialog-title"
@@ -93,19 +53,11 @@ class DeleteButton extends Component{
             <DialogTitle id="responsive-dialog-title" align = "center">Alert</DialogTitle>
             <DialogContent>
               <DialogContentText>
-<<<<<<< HEAD
-                Do you want to hide this item?
-              </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                        <Button variant = "contained" color = "secondary" onClick={this.handleHide}>
-=======
                 Are you sure you want to delete this item?
               </DialogContentText>
                         </DialogContent>
                         <DialogActions>
                         <Button variant = "contained" color = "secondary" onClick={this.handleDeletePost}>
->>>>>>> master
                            Yes
                         </Button>
                         <Button type="submit" variant="contained" color = "primary" onClick={this.handleClose}>
@@ -119,17 +71,9 @@ class DeleteButton extends Component{
     }    
 }
 
-<<<<<<< HEAD
-HideButton.propTypes = {
-    hideComment: PropTypes.func.isRequired,
-    unhideComment: PropTypes.func.isRequired,
-    hidePost: PropTypes.func.isRequired,
-    unhidePost: PropTypes.func.isRequired,
-=======
 DeleteButton.propTypes = {
     deleteComment: PropTypes.func.isRequired,
     deletePost: PropTypes.func.isRequired,
->>>>>>> master
     classes: PropTypes.object.isRequired,
     post: PropTypes.object.isRequired,
     postId: PropTypes.string.isRequired
@@ -139,14 +83,7 @@ const mapStateToProps = (state) => ({
     post: state.post.post,
 })
   const mapActionToProps ={ 
-<<<<<<< HEAD
-   hidePost, hideComment, unhideComment, unhidePost
-  }
-  
-export default connect(mapStateToProps,mapActionToProps)(withStyles(PostLayoutStyles)(HideButton));
-=======
    deletePost, deleteComment
   }
   
 export default connect(mapStateToProps,mapActionToProps)(withStyles(PostLayoutStyles)(DeleteButton));
->>>>>>> master
